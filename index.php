c964756fa8b8424a01afd131378590110cb67e19
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-04-02 12:48:56 +0100

    added basic form for inlogvenster
    input type text needs to be less width.

--- index.php
+++ index.php
@@ -23,9 +23,14 @@
       <div id="grid">
         <div id="logoDiv"><img src="Tools_For_Ever_Logo.png" alt="ToolsForEver_logo" id="logo"></div>
         <div id="tfeDiv"><span id="tfeText">ToolsForEver Vooraad</span></div>
-        <div id="medewerkerNaamDiv"><?php echo "<p>Medewerker: J. Hanssen</p>";?></div>
-        <div id="naamLabel"></div>
-        <div id="wachtwoordLabel"></div>
+        <!--<div id="medewerkerNaamDiv"><?php echo "<p>Medewerker: J. Hanssen</p>";?></div>-->
+        <form action="overzicht_medewerker.php" method="post">
+          <label for="naam" id="naamLabel">naam</label>
+          <input type="text" name="naam" value="" id="naamInput">
+          <label for="wachtwoord" id="wachtwoordLabel">wachtwoord</label>
+          <input type="text" name="wwoord" value="" id="wachtwoordInput">
+          <input type="submit" name="inlog" value="inloggen" id="inlogSubmit">
+        </form>
       </div>
     </div>
   </body>
