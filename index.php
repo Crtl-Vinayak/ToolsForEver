38017aa8d90429305ac2b04fb2176114ab137e51
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-04-05 15:07:36 +0100

    css files are now organized in a css style folder
    index (inlogvenster) needs message when "inlog" goes wrong.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <title>ToolsForEver voorraad opvragen</title>
   </head>
   <body>
@@ -26,9 +26,9 @@
         <!--<div id="medewerkerNaamDiv"><?php echo "<p>Medewerker: J. Hanssen</p>";?></div>-->
         <form action="overzicht.php" method="post">
           <label for="naam" id="naamLabel">naam</label>
-          <input type="text" name="naam" value="" id="naamInput">
+          <input type="text" name="naam" value="" id="naamInput" required>
           <label for="wachtwoord" id="wachtwoordLabel">wachtwoord</label>
-          <input type="password" name="wwoord" value="" id="wachtwoordInput">
+          <input type="password" name="wwoord" value="" id="wachtwoordInput" required>
           <input type="submit" name="inlog" value="inloggen" id="inlogSubmit">
         </form>
       </div>
--- overzicht.php
+++ overzicht.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style/overzicht.css">
     <title>ToolsForEver voorraad opvragen</title>
   </head>
   <body>
@@ -23,7 +23,7 @@
       <div id="grid">
         <div id="logoDiv"><img src="Tools_For_Ever_Logo.png" alt="ToolsForEver_logo" id="logo"></div>
         <div id="tfeDiv"><span id="tfeText">ToolsForEver Vooraad</span></div>
-        <!--<div id="medewerkerNaamDiv"><?php echo "<p>Medewerker: J. Hanssen</p>";?></div>-->
+        <div id="naamDiv"><?php echo "<p>Medewerker: J. Hanssen</p>";?></div>
         <form action="overzicht.php" method="post">
           <label for="naam" id="naamLabel">naam</label>
           <input type="text" name="naam" value="" id="naamInput">
